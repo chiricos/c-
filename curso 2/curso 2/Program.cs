336b173d86f44886eabc9396e594b982392ce3a1
vagrant <vagrant@homestead>
2018-05-18 19:44:40 +0000

    subiendo repositorio

---
+++ curso 2/curso 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //este es un comentario del programa
+            /*ESTE ES UN COMENTARIO
+             * EN BLOQUE */ 
+            Console.WriteLine("drawde");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ curso3/curso3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=0;
+            decimal x = 0.0m;
+            float f = 0.0f;
+            double d = 0.0D;
+            string cadena = "Hola mundo";
+            bool bandera = true;
+            DateTime fecha = DateTime.MinValue;
+            Console.WriteLine(bandera.ToString());
+            Console.WriteLine(fecha.ToShortDateString());
+            Console.ReadKey();
+
+        }
+    }
+}
