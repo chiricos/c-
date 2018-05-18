c148a278988d45b339a1738606c245cd3717500f
vagrant <vagrant@homestead>
2018-05-18 20:06:37 +0000

    subiendo curso 4

--- curso4/curso4/Program.cs
+++ curso4/curso4/Program.cs
@@ -10,6 +10,18 @@ namespace curso4
     {
         static void Main(string[] args)
         {
+            int i = 10;
+            decimal x = 12.2m;
+            bool bandera= true;
+            string cadena = string.Empty;
+            DateTime fecha =DateTime.MinValue;
+            i = (int)x;
+            Console.WriteLine("El valor de i es: {0} ", i);
+            Console.WriteLine("El valor de x es: {0:C}",x);
+            Console.WriteLine("El valor de bandera es: "+bandera.ToString());
+            Console.WriteLine("El valor de la cadena es: "+cadena);
+            Console.WriteLine("El valor de la fecha es:"+fecha.ToShortDateString());
+            Console.ReadKey();
         }
     }
 }
