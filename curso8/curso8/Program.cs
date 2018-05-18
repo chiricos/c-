24f26aec7dc9c5015f4ec8f404a6d9b9de235071
vagrant <vagrant@homestead>
2018-05-18 22:13:21 +0000

    subiendo curso 8 herencia

---
+++ curso8/curso8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            clsClientesVentas Cliente = new clsClientesVentas();
+            Cliente.IdCliente = 123;
+            Cliente.Apellidos = "Edward Diaz";
+            Cliente.Nombres = "Marcos";
+            Cliente.RFC = "HEHM";
+            Cliente.Direccion = "Calle 69 # 3 a 33 s";
+            Cliente.Colonia = "La gran via";
+            Cliente.Municipio = "bogota";
+            Cliente.EsCredito = false;
+            Console.WriteLine(Cliente.Apellidos + " " + Cliente.Nombres);
+            Console.WriteLine(Cliente.Direccion + " " + Cliente.Colonia + " " + Cliente.Municipio);
+            Console.WriteLine(Cliente.RFC);
+            if (Cliente.EsCredito)
+            {
+                Console.WriteLine("El cliente es de credito");
+            }
+            else
+            {
+                Console.WriteLine("El cliente no es de credito");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ curso8/curso8/clsClientes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso8
+{
+    public class clsClientes
+    {
+
+        private int _idCliente;
+        private string _Nombres;
+        private string _Apellidos;
+
+        public int IdCliente
+        {
+            get { return _idCliente; }
+            set { _idCliente = value; }
+        }
+        
+        public string Nombres
+        {
+            get { return _Nombres; }
+            set { _Nombres = value; }
+        }
+        
+        public string Apellidos
+        {
+            get { return _Apellidos; }
+            set { _Apellidos = value; }
+        }
+
+
+
+
+
+    }
+}
