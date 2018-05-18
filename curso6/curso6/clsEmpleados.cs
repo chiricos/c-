d113badbedaabc9881e0e4c84ff88140cf4a3f75
vagrant <vagrant@homestead>
2018-05-18 21:06:49 +0000

    subiendo curso 6 objetos y clases

---
+++ curso6/curso6/clsEmpleados.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso6
+{
+    public class clsEmpleados
+    {
+        public string Nombre;
+        public decimal sueldoDiario;
+        public int Edad;
+
+        public clsEmpleados()
+        {
+            Nombre = "";
+            sueldoDiario = 0.0m;
+            Edad = 0;
+        }
+
+        public decimal CalculaSalario(int NumeroDias)
+        {
+            return sueldoDiario * NumeroDias;
+        }
+    }
+}
