4e8e94c24e1b29c44023ad95630e61a1fb6cb09e
vagrant <vagrant@homestead>
2018-05-18 21:43:55 +0000

    subiendo curso 7 encalupsamiento

---
+++ curso7/curso7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal total;
+            clsEmpleados empleado = new clsEmpleados();
+            empleado.Edad = 25;
+            empleado.Nombre = "Edward Diaz";
+            empleado.SueldoDiario = 300000.0m;
+            total = empleado.CalculaSalario(30);
+            Console.WriteLine("El empleado: " + empleado.Nombre);
+            Console.WriteLine("El salario mensual es: " + total.ToString("C"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ curso7/curso7/clsEmpleados.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace curso7
+{
+    class clsEmpleados
+    {
+        private string _Nombre;
+        private int _Edad;
+
+        public clsEmpleados()
+        {
+            Nombre = "";
+            Edad = 0;
+        }
+
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public decimal SueldoDiario { get; set; }
+
+        
+        public decimal CalculaSalario(int NumeroDias)
+        {
+            return SueldoDiario * NumeroDias;
+        }
+    }
+}
